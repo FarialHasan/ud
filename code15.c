c16d8b78c1cc2092db1b700031f8ae164b02d037
Hasin Anjum <farialhasan9@gmail.com>
2025-03-21 20:29:34 +0600

    Create code15.c

---
+++ code15.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Formulas:\n");
+    printf("1. Area of Triangle: 0.5 * base * height\n");
+    printf("2. Area of Circle: π * r^2\n");
+    printf("3. Area of Rectangle: length * width\n");
+    printf("4. Celsius to Fahrenheit: (C * 9/5) + 32\n");
+    printf("5. Fahrenheit to Celsius: (F - 32) * 5/9\n");
+    
+    return 0;
+}
