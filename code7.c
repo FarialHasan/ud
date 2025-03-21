b84a082b1bbfff39fef2b4cc605d5c614dcff858
Hasin Anjum <farialhasan9@gmail.com>
2025-03-21 20:24:16 +0600

    Create code7.c

---
+++ code7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float cm, inch;
+    printf("Enter length in cm: ");
+    scanf("%f", &cm);
+    
+    inch = cm / 2.54;
+    printf("Length in inches: %.2f\n", inch);
+    
+    return 0;
+}
